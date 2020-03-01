f0b6081851b6ca4d2862cfe72f881eaad7986bbd
mattia <mattia.campestri@gmail.com>
2020-03-01 19:05:32 +0100

    Added functions to feature_matching

--- src/feature_detection.py
+++ src/feature_detection.py
@@ -1,6 +1,6 @@
-import cv2
+from cv2 import xfeatures2d
 
 
 def detect_features_SIFT(img):
-    sift = cv2.xfeatures2d.SIFT_create()
+    sift = xfeatures2d.SIFT_create()
     return sift.detectAndCompute(img, mask=None)
