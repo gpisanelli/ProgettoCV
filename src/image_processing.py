723659125568b7464eafd536f90f3cd8ca10c662
mattia <mattia.campestri@gmail.com>
2020-03-01 18:23:33 +0100

    Added functions to image_processing

---
+++ src/image_processing.py
@@ -0,0 +1,34 @@
+import cv2
+import numpy as np
+
+
+def equalize_histogram(img):
+    if len(img.shape) > 2:
+        return img
+    return cv2.equalizeHist(img)
+
+
+def sharpen_img(img):
+    blurred = cv2.GaussianBlur(img, (0, 0), 3)
+    return cv2.addWeighted(img, 1.5, blurred, -0.5, 0)
+
+
+def denoise_img(img):
+    return cv2.fastNlMeansDenoising(img, None, 10, 7, 21)
+
+
+
+def resize_img(img, scale_factor):
+    width = int(img.shape[1] * scale_factor)
+    height = int(img.shape[0] * scale_factor)
+    return cv2.resize(img, (width,height), interpolation=cv2.INTER_CUBIC)
+
+
+def resize_img_width(img, width):
+    height = int(img.shape[0] * width/img.shape[1])
+    return cv2.resize(img, (width,height), interpolation=cv2.INTER_CUBIC)
+
+
+def resize_img_height(img, height):
+    width = int(img.shape[1] * height/img.shape[0])
+    return cv2.resize(img, (width,height), interpolation=cv2.INTER_CUBIC)
