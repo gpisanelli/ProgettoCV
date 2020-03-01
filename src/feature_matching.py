a9d2a2b42c9c6991b7c9ef9dae8c00d0277bb8ed
mattia <mattia.campestri@gmail.com>
2020-03-01 19:22:59 +0100

    Changed feature_matching

--- src/feature_matching.py
+++ src/feature_matching.py
@@ -1,8 +1,7 @@
 import cv2
-import visualization
 
 
-def find_matches(k1, kp2, des1, des2):
+def find_matches(des1, des2):
 
     ########################## FLANN ##########################
     FLANN_INDEX_KDTREE = 1
@@ -10,7 +9,6 @@ def find_matches(k1, kp2, des1, des2):
     search_params = dict(checks=50)
     flann = cv2.FlannBasedMatcher(index_params, search_params)
     matches = flann.knnMatch(des1, des2, k=2)
-
 
     ########################## SIFT ##########################
     # bf = cv2.BFMatcher()
@@ -20,24 +18,10 @@ def find_matches(k1, kp2, des1, des2):
     # t = time.time() - start
     # print('\t\t\t\tTIME MATCHING: ', t)
 
-
-    # Draw matches
-    matchesMask = [[0, 0] for i in range(len(matches))]
-
-    for i, (match1, match2) in enumerate(matches):
-        if match1.distance < 0.7 * match2.distance:
-            matchesMask[i] = [1, 0]
-
-    draw_params = dict(matchColor=(0, 255, 0), singlePointColor=(255, 0, 0), matchesMask=matchesMask, flags=0)
-    flann_matches = cv2.drawMatchesKnn(source, kp1, scene, kp2, matches, None, **draw_params)
-    visualization.display_img(flann_matches, width=800, title='Matches')
-
-
     # Keep only good matches
     good_matches = []
     for match1, match2 in matches:
         if match1.distance < 0.7 * match2.distance:
             good_matches.append(match1)
-
 
     return good_matches
