c4e11beb24757d0d30c6909f9f5a304fcc6b747e
albjerto <albertojesu1995@gmail.com>
2020-03-10 16:18:16 +0100

    corrected barycenter, added joining vectors and accumulator

--- src/hough_detection.py
+++ src/hough_detection.py
@@ -1,30 +1,54 @@
 import cv2
+import numpy as np
 
 import image_processing
 import load_images
 import visualization
 
 
-def compute_barycenter(template):
-    # convert image to grayscale image
-    gray_image = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
-
-    # convert the grayscale image to binary image
-    ret, thresh = cv2.threshold(gray_image, 127, 255, 0)
-
-    # calculate moments of binary image
-    M = cv2.moments(thresh)
-
-    # calculate x,y coordinate of center
-    cX = int(M["m10"] / M["m00"])
-    cY = int(M["m01"] / M["m00"])
-
-    return cX, cY
+def compute_barycenter(keypoints):
+    x = np.mean([i.pt[0] for i in keypoints])
+    y = np.mean([i.pt[1] for i in keypoints])
+    return (x, y)
 
 
-def compute_GHT(matches):
-    pass    #WIP
+def compute_joining_vectors_table(matches, barycenter, keypoints):
+    joining_vectors_dict = {}
+    for match in matches:
+        # table element[sceneIndex] = templateIndex, vector_tuple
+        joining_vectors_dict[match.trainIdx] = [match.queryIdx,
+                                                (barycenter[0] - keypoints[match.queryIdx].pt[0],
+                                                 barycenter[1] - keypoints[match.queryIdx].pt[1])]
+    return joining_vectors_dict
 
+
+def compute_accumulator(joining_vectors, sceneImg_shape, kp_scene):
+    accumulator = np.zeroes(sceneImg_shape)
+    for i in range(len(kp_scene)):
+        accum_i, accum_j = joining_vectors[i][1][0] + kp_scene[i].pt[0], joining_vectors[i][1][1] + kp_scene[i].pt[1]
+        if accum_i < accumulator.shape[0] and accum_j < accumulator.shape[1]:
+            accumulator[accum_i, accum_j] += 1
+
+
+    return accumulator
+
+
+# matches lista dMatch -> ogni dMatch è un oggetto con 4 elementi ->
+# -> distance tra i keypoint
+# -> imageIndex (sempre 0)
+# -> queryIndex (indice del descriptor nella lista del template)
+# -> trainIndex (indice del descriptor nella lista della scena)
+# Dato che passiamo alla ricerca dei matches le liste dei descrittori
+# ottenute da sift e che queste hanno i descrittori nello stesso ordine in
+# cui sono i relativi keypoint (vedi sift.detectAndCompute()) ->
+# -> se manteniamo nella struttura dei joining vectors le corrispondenze
+# tra indici possiamo risalire accedendo con essi alle liste ottenute da
+# sift per ricavare i matching keypoint/descriptor -> possiamo quindi
+# proseguire col processo di votazione sulla scena
+
+
+# https://answers.opencv.org/question/18436/what-to-do-with-dmatch-value/
+# http://www.programmersought.com/article/3827388529/
 
 def main():
     hough = cv2.createGeneralizedHoughGuil()
@@ -48,13 +72,13 @@ def main():
     scene_path = '../images/generalized_hough_demo_01.png'
     scene = load_images.load_img_grayscale(scene_path)
 
-    hough.setTemplate(template, (int(template.shape[1]/2), int(template.shape[0]/2)))
+    hough.setTemplate(template, (int(template.shape[1] / 2), int(template.shape[0] / 2)))
     positions, votes = hough.detect(scene)
 
     for position in positions:
         print(position)
         cv2.polylines()
-        cv2.circle(scene, (int(position[0][0]),int(position[0][1])), 10, 100, 3)
+        cv2.circle(scene, (int(position[0][0]), int(position[0][1])), 10, 100, 3)
 
     visualization.display_img(scene)
 
